35cac113964af8a730b8a3cf8235d6251d743aa1
Gerardo Coronado <gcmarin9@gmail.com>
2023-11-28 15:15:25 -0400

    nuevos greetings

--- base-twilio-memory/tools/greetings.js
+++ base-twilio-memory/tools/greetings.js
@@ -3,6 +3,19 @@ function greetingsPool(name) {
     `Hola 👋🏻 *${name}*! un placer verte de nuevo por aquí`,
     `Que bueno verte de nuevo *${name}*! 😄`,
     `Por aquí *Ada* 👩‍🚀! Lista para ayudarte con lo que necesites *${name}*!`,
+    `¡Bienvenido *${name}*! ¿En qué puedo ayudarte hoy? 🌟`,
+    `¡Hola *${name}*! Estoy emocionado de verte de nuevo. 😊`,
+    `Saludos *${name}*! ¿Cómo ha sido tu día hasta ahora?`,
+    `¡Hola *${name}*! Espero que tengas un día maravilloso. 🌈`,
+    `¡Qué alegría verte *${name}*! ¿Cómo puedo asistirte hoy?`,
+    `Hola *${name}*! ¿Listo para comenzar?`,
+    `¡Hola *${name}*! Aquí estoy para ayudarte. 🚀`,
+    `¡Saludos *${name}*! ¿Qué novedades hay hoy?`,
+    `¡Hola *${name}*! ¿Cómo puedo hacer tu día mejor?`,
+    `¡Hola *${name}*! Estoy aquí para ayudarte en lo que necesites.`,
+    `¡Bienvenido *${name}*! ¿En qué puedo colaborar contigo hoy?`,
+    `¡Hola *${name}*! Espero que tu día esté yendo de maravilla. 😊`,
+    `¡Saludos *${name}*! ¿Hay algo en lo que pueda ayudarte hoy?`,
   ];
 
   return greetings[Math.floor(Math.random() * greetings.length)];
