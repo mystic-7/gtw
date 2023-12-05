36b75a217d479ce4ef487e66bafe5ba9070c7f64
Gerardo Coronado <gcmarin9@gmail.com>
2023-12-05 12:03:09 -0400

    limpieza de console.logs

--- base-twilio-memory/flows/tiendas.js
+++ base-twilio-memory/flows/tiendas.js
@@ -1,6 +1,11 @@
 const { addKeyword } = require('@bot-whatsapp/bot');
 const { airtableGet, airtableAnswers } = require('../services/airtable-client');
-const { createSortedList, getFlow, getFields,filterRecordsById } = require('../tools/utils');
+const {
+  createSortedList,
+  getFlow,
+  getFields,
+  filterRecordsById,
+} = require('../tools/utils');
 let city;
 
 const flowSucursales = addKeyword(['LISTA_DE_TIENDAS'], {
@@ -11,24 +16,46 @@ const flowSucursales = addKeyword(['LISTA_DE_TIENDAS'], {
     let store = parseInt(ctx.body);
     const sucursales = await airtableGet('sucursales');
     const tienda = getFlow(getFields(sucursales), store);
-    console.log(tienda)
-    console.log(getFields(sucursales))
     const flows = await airtableGet('flows');
     const disculpa = getFlow(getFields(flows), 'fallback');
-    console.log(tienda)
-    if (tienda === undefined){
+    if (tienda === undefined) {
       await flowDynamic(disculpa.texto);
       return fallBack();
     } else if (tienda.id_ciudad[0] === city) {
       await state.update({ sucursal: tienda.nombre });
       const myState = state.getMyState();
       airtableAnswers('conversaciones', myState, ctx);
-      var linkws = tienda.whalink ? tienda.whalink : ''
-      var motivacion = myState.motivo ? myState.motivo : ''
+      var linkws = tienda.whalink ? tienda.whalink : '';
+      var motivacion = myState.motivo ? myState.motivo : '';
       const listaDeContactos = await airtableGet('clientes');
-      const nombreDeContacto = filterRecordsById(listaDeContactos, ctx.from,true);
-      const correoDeContacto = filterRecordsById(listaDeContactos, ctx.from,false);
-      return await flowDynamic(tienda.direccion + '' + motivacion.toLowerCase() + '.\n' + '\n' + (linkws === '' ? '' : (linkws + `&text=Hola%20soy%20${nombreDeContacto.replace(/\s/g, '%20')},` + (correoDeContacto === undefined ? '' : ('%20mi%20correo%20es%20' + correoDeContacto + '%20y')) + `%20quisiera%20saber%20informaci%C3%B3n%20sobre%20${motivacion.replace(/\s/g, '%20')}`))); 
+      const nombreDeContacto = filterRecordsById(
+        listaDeContactos,
+        ctx.from,
+        true
+      );
+      const correoDeContacto = filterRecordsById(
+        listaDeContactos,
+        ctx.from,
+        false
+      );
+      return await flowDynamic(
+        tienda.direccion +
+          '' +
+          motivacion.toLowerCase() +
+          '.\n' +
+          '\n' +
+          (linkws === ''
+            ? ''
+            : linkws +
+              `&text=Hola%20soy%20${nombreDeContacto.replace(/\s/g, '%20')},` +
+              (correoDeContacto === undefined
+                ? ''
+                : '%20mi%20correo%20es%20' + correoDeContacto + '%20y') +
+              `%20quisiera%20saber%20informaci%C3%B3n%20sobre%20${motivacion.replace(
+                /\s/g,
+                '%20'
+              )}`)
+      );
     } else {
       await flowDynamic(disculpa.texto);
       return fallBack();
@@ -77,13 +104,40 @@ const flowTiendas = addKeyword(['LISTA_DE_CIUDADES'], {
         await state.update({ sucursal: ciudad.nombre_sucursales[0] });
         const myState = state.getMyState();
         airtableAnswers('conversaciones', myState, ctx);
-        var linkws = ciudad.whalink ? ciudad.whalink : ''
-        var motivacion = myState.motivo ? myState.motivo : ''
+        var linkws = ciudad.whalink ? ciudad.whalink : '';
+        var motivacion = myState.motivo ? myState.motivo : '';
         const listaDeContactos = await airtableGet('clientes');
-        const nombreDeContacto = filterRecordsById(listaDeContactos, ctx.from,true);
-        const correoDeContacto = filterRecordsById(listaDeContactos, ctx.from,false);
-        return await flowDynamic(ciudad.direccion[0] + ' ' + motivacion.toLowerCase() + '.\n' + '\n' + (linkws === '' ? '' : (linkws + `&text=Hola%20soy%20${nombreDeContacto.replace(/\s/g, '%20')},` + (correoDeContacto === undefined ? '' : ('%20mi%20correo%20es%20' + correoDeContacto + '%20y')) + `%20quisiera%20saber%20informaci%C3%B3n%20sobre%20${motivacion.replace(/\s/g, '%20')}`)));
-         
+        const nombreDeContacto = filterRecordsById(
+          listaDeContactos,
+          ctx.from,
+          true
+        );
+        const correoDeContacto = filterRecordsById(
+          listaDeContactos,
+          ctx.from,
+          false
+        );
+        return await flowDynamic(
+          ciudad.direccion[0] +
+            ' ' +
+            motivacion.toLowerCase() +
+            '.\n' +
+            '\n' +
+            (linkws === ''
+              ? ''
+              : linkws +
+                `&text=Hola%20soy%20${nombreDeContacto.replace(
+                  /\s/g,
+                  '%20'
+                )},` +
+                (correoDeContacto === undefined
+                  ? ''
+                  : '%20mi%20correo%20es%20' + correoDeContacto + '%20y') +
+                `%20quisiera%20saber%20informaci%C3%B3n%20sobre%20${motivacion.replace(
+                  /\s/g,
+                  '%20'
+                )}`)
+        );
       }
     }
   );
