baa437a7aaee0d1601a81f1ad1e2d4d3116b7b11
Lsas <lsascol01@gmail.com>
2024-03-19 12:27:21 -0400

    Correcion de error no flows

--- base-twilio-memory/flows/main-flows.js
+++ base-twilio-memory/flows/main-flows.js
@@ -144,6 +144,7 @@ const flowOpciones = addKeyword(['LISTA_DE_OPCIONES'], {
             return gotoFlow(flowTiendas);
           case 4:
             stopInactividad(ctx);
+            const flows = await airtableGet('flows');
             const promo_message = getFlow(getFields(flows), 'marketing_promo').texto
             if (promo_message.length > 1) {
               await flowDynamic(promo_message);
